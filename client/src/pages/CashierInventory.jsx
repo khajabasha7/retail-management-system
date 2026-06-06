7c09ea63eace33026f2ce3faedb394392a5260f8
khajabasha7 <shaikkhajabasha3@gmail.com>
2026-06-06 21:33:36 +0530

    week 3

--- client/src/pages/CashierInventory.jsx
+++ client/src/pages/CashierInventory.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import API from "../api/api";
+import "./CashierInventory.css";
+import {useNavigate} from "react-router-dom";
 
 function CashierInventory() {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchProducts();
@@ -18,10 +21,32 @@ function CashierInventory() {
   };
 
   return (
-    <div style={{ padding: "30px" }}>
-      <h1>Inventory</h1>
+  <div className="inventory-container">
 
-      <table border="1" cellPadding="10">
+    {/* Navbar */}
+    <div className="inventory-navbar">
+
+      {/* <button
+        className="back-btn"
+        onClick={() => navigate(-1)}
+      >
+        ← Back
+      </button> */}
+
+      <h2>Inventory Management</h2>
+
+      <button
+        className="home-btn"
+        onClick={() => navigate("/cashier")}
+      >
+        Back
+      </button>
+
+    </div>
+
+    <div className="inventory-content">
+
+      <table className="inventory-table">
         <thead>
           <tr>
             <th>Product</th>
@@ -40,16 +65,26 @@ function CashierInventory() {
               <td>₹{product.price}</td>
               <td>{product.stock}</td>
               <td>
-                {product.stock <= 5
-                  ? "Low Stock"
-                  : "Available"}
+                <span
+                  className={
+                    product.stock <= 5
+                      ? "low-stock"
+                      : "available"
+                  }
+                >
+                  {product.stock <= 5
+                    ? "Low Stock"
+                    : "Available"}
+                </span>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+
     </div>
-  );
+  </div>
+);
 }
 
 export default CashierInventory;
