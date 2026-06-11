15227114226d95e484f2c82fa47dfc592a0e146f
khajabasha7 <shaikkhajabasha3@gmail.com>
2026-06-11 15:46:52 +0530

    fix filename case

--- client/src/pages/Newsale.jsx
+++ client/src/pages/Newsale.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import API from "../api/api";
 import { useNavigate } from "react-router-dom";
-import "./Newsale.css";
+import "./NewSale.css";
 
 
 function NewSale() {
