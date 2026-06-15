af9bac0258f3aadeb42791b421e1941c6dfdda61
khajabasha7 <shaikkhajabasha3@gmail.com>
2026-06-15 18:45:17 +0530

    render update

--- server/server.js
+++ server/server.js
@@ -1,36 +1,96 @@
+
 // const express = require("express");
 // const cors = require("cors");
 // const mongoose = require("mongoose");
 // require("dotenv").config();
 
 
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerSpec = require("./swagger");
 
 
 // const authRoutes = require("./routes/authRoutes");
+// const productRoutes = require("./routes/productRoutes");
+// const saleRoutes = require("./routes/saleRoutes");
+
+
 
 // const app = express();
 
+
+// // middleware
+
 // app.use(cors());
+// // app.use(cors({
+// //   origin: [
+// //     "http://localhost:5173",
+// //     "https://your-real-vercel-url.vercel.app"
+// //   ],
+// //   credentials: true
+// // }));
+
 // app.use(express.json());
 
-// const productRoutes = require("./routes/productRoutes");
 
-// app.use("/api/products", productRoutes);
 
-// const saleRoutes = require("./routes/saleRoutes");
+// // Swagger
+// app.use(
+//   "/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerSpec)
+// );
 
-// app.use("/api/sales", saleRoutes);
+
+
+// // Routes
+
+// app.use(
+//   "/api/products",
+//   productRoutes
+// );
+
+
+// app.use(
+//   "/api/sales",
+//   saleRoutes
+// );
+
+
+// app.use(
+//   "/api/auth",
+//   authRoutes
+// );
+
+
+
+// // MongoDB
 
 // mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch(err => console.log(err));
 
-// app.use("/api/auth", authRoutes);
+// .then(()=>{
 
-// app.listen(5000, () => {
-//   console.log("Server running on port 5000");
+// console.log("MongoDB Connected");
+
+// })
+
+// .catch(err=>{
+
+// console.log(err);
+
 // });
 
+
+
+
+// // Server
+
+// app.listen(5000,()=>{
+
+// console.log(
+// "Server running on port 5000"
+// );
+
+// });
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -46,26 +106,24 @@ const productRoutes = require("./routes/productRoutes");
 const saleRoutes = require("./routes/saleRoutes");
 
 
-
 const app = express();
 
 
-// middleware
+// Middleware
 
-app.use(cors());
-// app.use(cors({
-//   origin: [
-//     "http://localhost:5173",
-//     "https://your-real-vercel-url.vercel.app"
-//   ],
-//   credentials: true
-// }));
+app.use(
+  cors({
+    origin: "*"
+  })
+);
+
 
 app.use(express.json());
 
 
 
 // Swagger
+
 app.use(
   "/api-docs",
   swaggerUi.serve,
@@ -101,25 +159,41 @@ mongoose.connect(process.env.MONGO_URI)
 
 .then(()=>{
 
-console.log("MongoDB Connected");
+ console.log("MongoDB Connected");
 
 })
 
-.catch(err=>{
+.catch((err)=>{
 
-console.log(err);
+ console.log("MongoDB Error:",err);
 
 });
 
 
 
+// Test route
+
+app.get("/",(req,res)=>{
+
+ res.send("Retail Management Backend Running");
+
+});
+app.get("/test",(req,res)=>{
+ res.json({
+  message:"Render working"
+ });
+});
+
 
 // Server
 
-app.listen(5000,()=>{
+const PORT = process.env.PORT || 5000;
 
-console.log(
-"Server running on port 5000"
-);
+
+app.listen(PORT,()=>{
+
+ console.log(
+  `Server running on port ${PORT}`
+ );
 
 });
